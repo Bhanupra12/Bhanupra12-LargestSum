735307d06479dac8c74450cfcacf0cd49d7322aa
Bhanupra12 <bhanuprakashthokala12@gmail.com>
2025-05-07 13:25:13 +0530

    code

--- src/main/java/LargestSum.java
+++ src/main/java/LargestSum.java
@@ -9,7 +9,31 @@ public class LargestSum {
      * @param nums a list of ints.
      * @return the largest possible sum of separate numbers from nums.
      */
-    public int bigSum(List<Integer> nums){
-        return 0;
+    public int bigSum(List<Integer> nums){if (nums == null || nums.size() < 2) {
+        throw new IllegalArgumentException("List must contain at least two numbers.");
+    }
+
+    int max1 = Integer.MIN_VALUE;
+    int max2 = Integer.MIN_VALUE;
+    int max1Count = 0;
+
+    for (int num : nums) {
+        if (num > max1) {
+            max2 = max1;
+            max1 = num;
+            max1Count = 1;
+        } else if (num == max1) {
+            max1Count++;
+        } else if (num > max2) {
+            max2 = num;
+        }
+    }
+
+    // If the largest number occurs at least twice, we can use it twice.
+    if (max1Count >= 2) {
+        return max1 + max1;
+    } else {
+        return max1 + max2;
+    }
     }
 }
